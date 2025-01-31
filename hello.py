3e3d909b855e8c24c2baae2296ded3ec1071094f
tphuntsho2001 <tashiphuntsho@dhi.bt>
2025-01-31 10:39:35 +0000

    changed main and test

--- hello.py
+++ hello.py
@@ -1,7 +1,5 @@
 def add(x,y):
     return int(x+y)
 
-print(add(1,1))
-
-var = 10
-var = var
+def subtract(a,b):
+    return int(a-b)
--- test_hello.py
+++ test_hello.py
@@ -1,5 +1,9 @@
 from hello import add
-
+from hello import subtract
 def test_addition():
     assert 2 == add(1,1)
 
+def test_subtraction():
+    assert 0 == subtract(2,2)
+
+    
